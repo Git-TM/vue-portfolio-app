173041417d764423ca6244bb42cc087bd0d9cc0b
Tristan Monteiro <tristan.monteiro@captive.fr>
2023-07-06 17:30:39 +0200

    ✨ Initial Commit + test v-for

---
+++ src/components/MainCard.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div class="main-card">
+    <div class="container-one">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <h2>Tristan</h2>
+      <p>Développeur Vue.js</p>
+    </div>
+    <div class="container-two">
+      <p>Je suis dev Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+  .main-card {
+    display: flex;
+    align-items: center;
+    background-color: aqua;
+  }
+  .container-one{
+    width: 50%;
+    margin: auto;
+    margin: 30px 0px;
+  }
+</style>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,36 @@
+<template lang="">
+<div class="projects-card">
+  <div v-for="(item, index) in projectData.ProjectsArray" :key="index" class="project-card">
+    <img :src="projectData.ProjectsArray[index].link1" alt="">
+    <h2>{{projectData.ProjectsArray[index].nom}}</h2>
+    <p>{{projectData.ProjectsArray[index].subject}}</p>
+  </div>
+</div>
+</template>
+<script>
+import jsonProjects from "/project.json";
+export default {
+  data(){
+    return{
+      projectData: jsonProjects
+    }
+  }
+}
+</script>
+<style scoped>
+  .projects-card{
+    display:flex;
+    margin: auto;
+  }
+  .project-card{
+    margin: 20px;
+    width: 400px;
+    height: 400px;
+    background-color: grey;
+  }
+
+  img {
+    width: 100%;
+    height: 150px;
+  }
+</style>
